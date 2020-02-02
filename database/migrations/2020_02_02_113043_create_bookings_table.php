88d2af937fcb998d641e0f4af21b3e22b08439d2
Natthapon Sricort <natthaponsricort@gmail.com>
2020-02-02 21:43:39 +0700

    Init

---
+++ database/migrations/2020_02_02_113043_create_bookings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type');
+            $table->string('room_number');
+            $table->date('check_in');
+            $table->date('check_out');
+            $table->string('customer');
+            $table->string('nationality');
+            $table->string('market_segment')->nullable();
+            $table->decimal('total_price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
